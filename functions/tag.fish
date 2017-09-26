ad5c015e3e79c0daeeac3c72f4341d5396c47f7e
王镇 <hero21c@outlook.com>
2017-09-26 21:54:13 +0800

    新增 tag

--- functions/tag.fish
+++ functions/tag.fish
@@ -1,5 +1,6 @@
 function tag
-        if __has_argv $argv
+        # __test
+        if count $argv > /dev/null
                 if __has_commit
                         __add_version $argv
                 end                        
@@ -9,7 +10,6 @@ function tag
 end
 
 function __add_version
-        set -l v_current (__get_last_version)
         set -l versions (string split . $v_current)
         
         set -g v_primary $versions[1]
@@ -23,26 +23,34 @@ function __add_version
         end
         
         set v_new $v_primary.$v_function.$v_bugfix
+        
         set -l msgs (__commit_messages $v_current)
-        echo "新版本为：" $v_new
+        echo "新版本为：" $v_new "msg: $msgs"
         eval "git tag -a v$v_new -m $msgs"
 end
 
 
 function __get_last_version
+        # echo "__get_last_version"
         if __has_tag
                 set tags (git tag -l)
                 set -l lastIndex (math (count $tags))
                 set -l last_tag $tags[$lastIndex]
-                echo (string sub -s 2 $last_tag) # remove v
+                set -g v_current (string sub -s 2 $last_tag) # remove v
         else
-                echo "0.0.0"
+                set -g v_current "0.0.0"
         end
 end
 
-
-function __has_argv
-        return (count $argv > /dev/null)
+function __has_tag
+        set tags (git tag -l)
+        set tag_length (math (count $tags))
+        echo $tag_length
+        if math "$tag_length == 0"
+                return (__bool_false)
+        else
+                return (__bool_true)
+        end                
 end
 
 function __is_bug_fix
@@ -55,7 +63,7 @@ function __is_bug_fix
 end
 
 function __has_commit
-        set msg (__commit_messages (__get_last_version))
+        # set msg (__commit_messages (__get_last_version))
         set length (string length (string trim $msg))
 
         if math "$length == 0"
@@ -66,30 +74,19 @@ function __has_commit
 end
 
 function __commit_messages
-        
         if __has_tag
-                set msg (eval "git log --pretty=format:'%s' v$argv...HEAD")
+                set -g msg (eval "git log --pretty=format:'%s' v$argv...HEAD")
         else
-                set msg (eval "git log --pretty=format:'%s' HEAD")
+                set -g msg (eval "git log --pretty=format:'%s' HEAD")
         end
-        
-        echo $msg
 end
 
-function __has_tag
-        set tags (git tag -l)
-        set tag_length (math (count $tags))
-        if math "$tag_length != 0"
-                return (__bool_true)
-        else
-                return (__bool_false)
-        end                
+
+function __bool_true
+        return 1
 end
 
 function __bool_false
-        return 1
-end
-function __bool_true
         return 0
 end
 
