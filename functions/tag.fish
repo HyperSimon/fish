88e8aa444407b10ed3893dfad3db7e38af5e5bce
HyperSimon <hero21c@outlook.com>
2017-10-28 23:08:54 +0800

    修改 tag的message为上一条tag到目前提交的message的集合

--- functions/tag.fish
+++ functions/tag.fish
@@ -1,10 +1,11 @@
 function tag
         if count $argv > /dev/null
-                
+
+                # 设置版本号
+                __ensure_version
                 for i in (seq (count $argv))
-
+                        
                         switch $argv[$i]
-
                                 # 获取新的版本号
                                 case -m --major
                                         set -g newVersion (__update_major)
@@ -14,12 +15,15 @@ function tag
                                         set -g newVersion (__update_patch)
                         end
                         
-                        echo $newVersion 
-                        eval "git tag $newVersion"
+                        echo "新建版本:"$newVersion
+                        set -l message (__make_message)
+                        echo "新增修复:"
+                        echo $message
+                        
+                        eval "git tag -a $newVersion -m \"$message\""
                 end
 
         end
-        
 end
 
 
@@ -36,34 +40,37 @@ function __get_subversion
         echo $versions[$argv]
 end
 
+function __ensure_version
+        set -g major (__get_subversion 1)
+        set -g minor (__get_subversion 2)       
+        set -g patch (__get_subversion 3) 
+end
+
 
 # | major | minor | patch |
+# major.minor.patch
 # 大版本
 function __update_major
-        set -l major (__get_subversion 1)
-        set -l minor (__get_subversion 2)       
-        set -l patch (__get_subversion 3) 
-
-        set -l new_major (math $major + 1)
-        echo v$new_major.$minor.$patch
+        set -g major (math $major + 1)
+        echo v$major.$minor.$patch
 end
 
 # 功能更新
 function __update_minor
-        set -l major (__get_subversion 1)
-        set -l minor (__get_subversion 2)       
-        set -l patch (__get_subversion 3) 
-
-        set -l new_minor (math $minor + 1)
-        echo v$major.$new_minor.$patch
+        set -g minor (math $minor + 1)
+        echo v$major.$minor.$patch
 end
 
 # 补丁
 function __update_patch
-        set -l major (__get_subversion 1)
-        set -l minor (__get_subversion 2)       
-        set -l patch (__get_subversion 3) 
-
-        set -l new_patch (math $patch + 1)
-        echo v$major.$minor.$new_patch
+        set -g patch (math $patch + 1)
+        echo v$major.$minor.$patch
 end
+
+function __make_message
+        set -l tags (git tag -l --sort=v:refname)
+        set -l lastTag $tags[(count $tags)]
+        set -l message (git log --pretty=format:"%s" --no-merges $lastTag..)
+        echo $message
+end
+
