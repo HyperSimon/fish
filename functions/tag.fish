e69e6b39beb21d9c80ea93bcb21a270092e62f60
HyperSimon <hero21c@outlook.com>
2017-10-28 21:28:37 +0800

    修改 tag

--- functions/tag.fish
+++ functions/tag.fish
@@ -1,92 +1,69 @@
 function tag
-        # __test
         if count $argv > /dev/null
-                if __has_commit
-                        __add_version $argv
-                end                        
-        else
-                echo "请输入参数"
+                
+                for i in (seq (count $argv))
+
+                        switch $argv[$i]
+
+                                # 获取新的版本号
+                                case -m --major
+                                        set -g newVersion (__update_major)
+                                case -f --feature
+                                        set -g newVersion (__update_minor)
+                                case -b --bug
+                                        set -g newVersion (__update_patch)
+                        end
+                        
+                        echo $newVersion 
+                        eval "git tag $newVersion"
+                end
+
         end
+        
 end
 
-function __add_version
-        set -l versions (string split . $v_current)
-        
-        set -g v_primary $versions[1]
-        set -g v_function $versions[2]
-        set -g v_bugfix $versions[3]
-
-        if __is_bug_fix $argv  # add bug version
-                set v_bugfix (math $v_bugfix + 1)
-        else # add function version
-                set v_function (math $v_function + 1)
-        end
-        
-        set v_new $v_primary.$v_function.$v_bugfix
-        
-        set -l msgs (__commit_messages $v_current)
-        echo "新版本为：" $v_new "msg: $msgs"
-        eval "git tag -a v$v_new -m $msgs"
-end
 
 
 function __get_last_version
-        # echo "__get_last_version"
-        if __has_tag
-                set tags (git tag -l)
-                set -l lastIndex (math (count $tags))
-                set -l last_tag $tags[$lastIndex]
-                set -g v_current (string sub -s 2 $last_tag) # remove v
-        else
-                set -g v_current "0.0.0"
-        end
+        set -l versions (string split ' ' (git tag -l --sort=v:refname))
+        set -l lastVersion (string sub -s 2 $versions[(count $versions)])
+        echo $lastVersion
 end
 
-function __has_tag
-        set tags (git tag -l)
-        set tag_length (math (count $tags))
-        echo $tag_length
-        if math "$tag_length == 0"
-                return (__bool_false)
-        else
-                return (__bool_true)
-        end                
-end
-
-function __is_bug_fix
-        switch $argv[1]
-                case -f --funciton
-                        return 1 # 1 == false
-                case -b --bug    
-                        return 0 # 0 == true
-        end
-end
-
-function __has_commit
-        # set msg (__commit_messages (__get_last_version))
-        set length (string length (string trim $msg))
-
-        if math "$length == 0"
-                return (__bool_false)
-        else
-                return (__bool_true)
-        end
-end
-
-function __commit_messages
-        if __has_tag
-                set -g msg (eval "git log --pretty=format:'%s' v$argv...HEAD")
-        else
-                set -g msg (eval "git log --pretty=format:'%s' HEAD")
-        end
+function __get_subversion
+        set -l lastVersion (__get_last_version)
+        set -l versions (string split . $lastVersion)
+        echo $versions[$argv]
 end
 
 
-function __bool_true
-        return 1
+# | major | minor | patch |
+# 大版本
+function __update_major
+        set -l major (__get_subversion 1)
+        set -l minor (__get_subversion 2)       
+        set -l patch (__get_subversion 3) 
+
+        set -l new_major (math $major + 1)
+        echo v$new_major.$minor.$patch
 end
 
-function __bool_false
-        return 0
+# 功能更新
+function __update_minor
+        set -l major (__get_subversion 1)
+        set -l minor (__get_subversion 2)       
+        set -l patch (__get_subversion 3) 
+
+        set -l new_minor (math $minor + 1)
+        echo v$major.$new_minor.$patch
 end
 
+# 补丁
+function __update_patch
+        set -l major (__get_subversion 1)
+        set -l minor (__get_subversion 2)       
+        set -l patch (__get_subversion 3) 
+
+        set -l new_patch (math $patch + 1)
+        echo v$major.$minor.$new_patch
+end
